f0b493fa15c059da6c05f661338390861f4dedb5
SymonMuchemi <simonkenya846@gmail.com>
2023-08-14 22:15:14 +0300

    Graphs

---
+++ src/com/symon/Node.java
@@ -0,0 +1,31 @@
+package com.symon;
+
+public class Node {
+    char data;
+
+    Node(char data){
+        this.data = data;
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(4, 0);
+        graph.addEdge(4, 2);
+
+        graph.printMatrix();
+
+        System.out.println(graph.checkEdge(0,2));
+    }
+}
