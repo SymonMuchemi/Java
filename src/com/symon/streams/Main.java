e962b053357b42d7aa9c1c4e49be9c1e746395ea
SymonMuchemi <simonkenya846@gmail.com>
2023-09-15 21:30:42 +0300

    Streams: filter()

--- src/com/symon/streams/Main.java
+++ src/com/symon/streams/Main.java
@@ -1,33 +1,79 @@
 package com.symon.streams;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(
+                10, 15, 20, 30, 35, 40
+        );
+        List<Integer> evenNumbers;
 
-        List<Person> people = getPeople();
+        // Without using streams
+        /*
+        for (Integer number : numbers) {
+            if (number % 2 == 0){
+                evenNumbers.add(number);
+            }
+        }
+        System.out.println(evenNumbers);
+*/
+        // using stream concept - with filter method
+        evenNumbers =  numbers.stream().filter(n -> n % 2 ==0)
+                .collect(Collectors.toList());
+        System.out.println(evenNumbers);
 
-        // Filtering the sequence of objects
+        System.out.println("Numbers only divisible by 5 and 3");
+        // Without assigning the resulting collection to a variable
+        numbers.stream().filter(n -> (n % 5 == 0) && (n % 2 != 0)).forEach(System.out::println);
 
-        List<Person> females = people.stream()
-                .filter(person -> person.getGender().equals(Gender.FEMALE))
+        // Example 2
+        System.out.println("Example 2");
+        List<String> names = Arrays.asList(
+                "Simon",
+                "Jeremiah",
+                "Nebuchadnezzar",
+                "Disney",
+                "Franklin",
+                "Churchill"
+        );
+        //goal: retrieve the names of length greater than 6 and less than 8
+        List<String> longNames;
+
+        longNames = names.stream().filter(name ->
+                (name.length() >= 6))
+                .collect(Collectors.toList());
+        System.out.println("\nLong names:\n");
+        longNames.forEach(System.out::println);
+
+        System.out.println("\nRemoving NULL inside a collection\n");
+        List<String> words = Arrays.asList(
+                "Melon",
+                null,
+                "Juice",
+                "Top",
+                "Jump",
+                null,
+                null,
+                "Good"
+        );
+
+        // Filtering and collecting
+        List<String> wordsWithoutNulls = words.stream()
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
 
-        females.forEach(System.out::println);
+        System.out.println("Initial list with null values" );
+        words.forEach(System.out::println);
+        System.out.println("\nAfter filtering");
+        wordsWithoutNulls.forEach(System.out::println);
 
-    }
-
-    private static List<Person> getPeople() {
-        return List.of(
-                new Person("Antonio", 20, Gender.MALE),
-                new Person("Alina Smith", 33, Gender.FEMALE),
-                new Person("Helen White", 57, Gender.FEMALE),
-                new Person("Alex Boz", 14, Gender.MALE),
-                new Person("Jamie Goa", 99, Gender.MALE),
-                new Person("Anna Cook", 7, Gender.FEMALE),
-                new Person("Zelda Brown", 120, Gender.FEMALE)
-        );
+        // filtering without collecting
+        System.out.println();
+        words.stream().filter(Objects::nonNull).forEach(System.out::println);
     }
 
 }
