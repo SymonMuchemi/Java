f052752a00b9c8b66a31a9fcdb813b3942d48481
SymonMuchemi <simonkenya846@gmail.com>
2023-09-20 00:25:16 +0300

    Parallel streams: using sequential streams

---
+++ src/com/symon/streams/parallelStreams/Demo1.java
@@ -0,0 +1,49 @@
+package com.symon.streams.parallelStreams;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Student{
+    String name;
+    int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+}
+public class Demo1 {
+    public static void main(String[] args) {
+        /*
+            Goal: create a bunch of student objects, filter according to
+            their scores limit the number and retrieve the result
+        */
+        List<Student> studentList;
+
+        studentList = Arrays.asList(
+                new Student("James", 55),
+                new Student("Bob", 90),
+                new Student("John", 65),
+                new Student("Erick", 82),
+                new Student("Susan", 88),
+                new Student("Scott", 90)
+        );
+        // filter the students with scores greater than 80
+        // method 1
+
+        studentList.stream().filter(student -> student.score > 80)
+                    .limit(3)
+                    .forEach(student -> {
+                        System.out.printf("student = %s score -> %d\n", student.getName(), student.getScore() );
+                    });
+    }
+}
