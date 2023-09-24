b61f3c52f37b7a0db23ae41255cf3730f56eec32
SymonMuchemi <simonkenya846@gmail.com>
2023-09-24 22:47:15 +0300

    Checking the name of the current thread

--- src/com/symon/threads/Main.java
+++ src/com/symon/threads/Main.java
@@ -4,6 +4,9 @@ public class Main {
     // a thread is a light weight sub-process
     // it is the smallest independent unit of a program
     // every java program contains at least one thread
-    //  a thread is created and controlled by the java.lang.Thread class
-
+    // a thread is created and controlled by the java.lang.Thread class
+    public static void main(String[] args) {
+//        System.out.print(Thread.activeCount());
+        System.out.print(Thread.currentThread().getName());
+    }
 }
