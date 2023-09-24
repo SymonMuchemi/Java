efc1005771a94d79aefec9c9e462be76ef5ce7e6
SymonMuchemi <simonkenya846@gmail.com>
2023-09-24 22:46:47 +0300

    set username and password to public

--- src/com/symon/deserialization/User.java
+++ src/com/symon/deserialization/User.java
@@ -3,8 +3,8 @@ package com.symon.deserialization;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    String name;
-    String password;
+    public String name;
+    public String password;
 
     public void sayHello(){
         System.out.println("Hello " + name);
