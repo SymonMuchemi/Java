6b1817d3dd1e9ae902114bd45b0decc18c9d32c9
SymonMuchemi <simonkenya846@gmail.com>
2023-09-27 13:01:51 +0300

    Simple echo application

--- src/com/symon/sockets/Client.java
+++ src/com/symon/sockets/Client.java
@@ -1,14 +1,35 @@
 package com.symon.sockets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Client started");
+        String string;
+        BufferedReader userInput;
+        BufferedReader socketInput;
+        PrintWriter out;
 
         try {
+            System.out.println("Client started");
             Socket socket = new Socket("localhost", 9806);
+
+            //read data from the keyboard
+            userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter a string");
+            string = userInput.readLine();
+
+            if (string != null){
+                out  = new PrintWriter(socket.getOutputStream(), true);
+                out.println(string);
+                socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                System.out.println(socketInput.readLine());
+            } else {
+                System.out.println("Cannot send an empty string!!");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/com/symon/sockets/Server.java
+++ src/com/symon/sockets/Server.java
@@ -1,17 +1,41 @@
 package com.symon.sockets;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        System.out.println("Server socket: waiting for a client");
+        ServerSocket serverSocket;
+        Socket socket;
+        BufferedReader buffReadIn;
+        String receivedStr;
+        PrintWriter out;
 
         try {
-            ServerSocket serverSocket = new ServerSocket(9806);
-            Socket socket = serverSocket.accept();
+            System.out.println("Server socket: waiting for a client...");
+
+            //noinspection resource
+            serverSocket = new ServerSocket(9806);
+            socket = serverSocket.accept();
             System.out.println("Connection established!");
+
+            // get input string from the socket
+            buffReadIn = new BufferedReader(
+                    new InputStreamReader(
+                            socket.getInputStream()
+                    )
+            );
+
+            receivedStr = buffReadIn.readLine();
+            receivedStr += " (I've read it!)";
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            out.println("Server says: " + receivedStr);
         } catch (Exception e){
+            //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
     }
